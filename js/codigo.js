e40618d9bc1f6383c3df007500edfe4afe24e285
nuinve <163067356+nuinve@users.noreply.github.com>
2024-05-19 18:13:34 -0300

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,9 @@
+function validacao(){
+    let nome = document.getElementById("nome");    
+    let acesso = document.getElementById("acesso");
+    let disponivel = document.getElementById("disponivel");
+    let ferramentas = document.getElementById("ferramentas");
+    let contato = document.getElementById("contato");
+
+    
+}
